9b752073583b60e5767f6c8c80384303a00fa023
SiddheshKotwal <121744452+SiddheshKotwal@users.noreply.github.com>
2023-06-13 13:15:47 +0530

    Update small_homepage.js

--- small_homepage.js
+++ small_homepage.js
@@ -94,6 +94,21 @@ document.addEventListener('DOMContentLoaded', function(){
       document.getElementById('submit').addEventListener('click', function(){
         console.log('Message Sent!');
       });
+    
+    document.onkeydown = function(e) {
+        if(event.keyCode == 123) {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+            return false;
+        }
+        if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+            return false;
+        }
+    }
 
 });
 
